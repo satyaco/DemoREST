a404de1b6ff80aafad42a88f5ff6bf98d5fa97bc
satyaco <amsatymail@gmail.com>
2019-09-12 18:03:44 +0530

    bootjpa project commit

---
+++ bootjpa/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101, 'Satya', 'Java');
+insert into alien values(102, 'Sumit', 'PHP');
+insert into alien values(103, 'Karthika', 'Java');
+insert into alien values(104, 'Prabha', 'DotNet');
+insert into alien values(105, 'Ravi', 'Android');
